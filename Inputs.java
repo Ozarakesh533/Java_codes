390702fba9cecda779ab4819715db7d5c65f163c
Rakesh Oza <90272721+Ozarakesh533@users.noreply.github.com>
2023-10-01 18:34:51 +0530

    Add files via upload

---
+++ Inputs.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        System.out.println("Hey how are you?");
+        Scanner input=new Scanner(System.in);
+    }
+}
---
+++ Primitives.java
@@ -0,0 +1,11 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 24;
+        char letter = 'R';
+        float marks = 98.67f;
+        double largeDecimalNumber = 4567654.4567;
+        boolean chech = false;
+
+    }
+}
+
